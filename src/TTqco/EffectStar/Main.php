82d9585a84f4f6e6772dece809a2b93045d78452
Mack <61392202+goosebumps98@users.noreply.github.com>
2020-09-19 16:42:29 +0800

    Update Main.php

--- src/TTqco/EffectStar/Main.php
+++ src/TTqco/EffectStar/Main.php
@@ -22,7 +22,7 @@ class Main extends PluginBase implements Listener {
 
 public function onEnable() {
 $this->getServer()->getPluginManager()->registerEvents($this,$this);
-$this->getLogger()->info(TextFormat::GREEN . "EffectStar Is Online");
+$this->getLogger()->info(TextFormat::GREEN . "Junlge Boost Started");
 }
 
 
